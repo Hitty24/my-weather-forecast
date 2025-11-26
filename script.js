2207e89f2f9e215115936eb89955503eb1b6f254
Hitty24 <135399498+Hitty24@users.noreply.github.com>
2025-11-26 19:48:43 +0200

    added more weather data functionality

--- script.js
+++ script.js
@@ -2,10 +2,43 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time *1000)
+
+
+  console.log(response.data);
+ 
+
 
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
+
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+   windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+ temperatureElement.innerHTML = Math.round(temperature);
+
 }
+
+
+function formatDate(date) {
+   
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let day = days [date.getDay()];
+    if (minutes < 10) {
+        minutes= `0${minutes}`;
+    }
+
+return `${day}, ${hours}: ${minutes}`;
+} 
+
+
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
