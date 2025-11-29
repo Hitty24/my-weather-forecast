2f315943d2557444075ddc79848311982d6a7f16
Hitty24 <135399498+Hitty24@users.noreply.github.com>
2025-11-29 11:44:37 +0200

    fixed and improved Html, CSS, and Js. also injected the forecast HTML into JavaScript

--- script.js
+++ script.js
@@ -4,39 +4,40 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windspeedElement = document.querySelector("#wind-speed");
+  let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time *1000);
+  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-   icon.innerHTML = ` <img src="${response.data.condition.icon_url}"class="weather-app-icon" >`
- 
-
- cityElement.innerHTML = response.data.city;
-
+  cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-   windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
- temperatureElement.innerHTML = Math.round(temperature);
-
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
-   
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-    let day = days [date.getDay()];
-    if (minutes < 10) {
-        minutes= `0${minutes}`;
-    }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-return `${day}, ${hours}: ${minutes}`;
-} 
-
-
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
@@ -51,7 +52,43 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function diaplayForecast() {
+
+
+
+
+let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+let forecastHtml = "";
+
+days.forEach(function (day) {
+  forecastHtml = 
+  forecastHtml +
+
+  `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>10º</strong>
+              </div>
+              <div class="weather-forecast-temperature">9º</div>
+            </div>
+          </div>
+          `;
+        });
+        
+        let forecastElement = document.querySelector("#forecast");
+        forecastElement.innerHTML = forecastHtml
+
+
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("polokwane");
+
+diaplayForecast();
+
+
+
